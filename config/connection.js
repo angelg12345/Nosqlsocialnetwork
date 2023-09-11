07c656459db44859e1f805695bb73746b18123a6
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-11 16:33:36 -0700

    added error messages for my connection file and started on my models

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,19 @@ const connectionString =
 process.env.MONGODB_URI || 'mongodb://localhost:27017/social_network_db';
 
 mongoose.connect(connectionString, {
-    useNewURLParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindandModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.error('Error connecting to mongodb:', error);
+});
+
+db.once('open', () => {
+    console.log('Connected to mongoDB')
 });
 
 module.exports = connection; 
