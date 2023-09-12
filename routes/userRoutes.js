a853d21f2253472e90302f5c9beedab5fb622339
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-11 18:18:35 -0700

    finished thoughtRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../controllers')
+
+const router = express.Router();
+
+router.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+router.route(':id')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(removeFriend);
+
+module.exports = router;
