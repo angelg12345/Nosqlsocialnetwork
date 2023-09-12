2487de1593627e91e375dffc4c49eb39ac2d8094
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-11 17:30:41 -0700

    made my user.js model

--- models/User.js
+++ models/User.js
@@ -4,11 +4,33 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
     },
-})
+    thoughts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
